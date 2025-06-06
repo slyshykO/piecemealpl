9e7363644e6b509c42de37ed767b5c867ca08301
Oleksiy Slyshyk <alexSlyshyk@gmail.com>
2025-06-06 09:26:48 +0300

    cmdline rust: remove ambiguous collection to vector

--- cmdlineargs/rust/src/main.rs
+++ cmdlineargs/rust/src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    for item in args.into_iter().enumerate() {
+    for item in env::args().enumerate() {
         let (_i, x): (usize, String) = item;
         println!("{x}");
     }
